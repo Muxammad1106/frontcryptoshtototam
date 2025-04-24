110bb23acd24871c9a8839d5b6814fd285a6ba82
Muxammad1106 <muxammadzoxid110615@gmail.com>
2025-04-24 07:18:25 +0500

    Исправлена ошибка сборки: добавлен terser и обновлены настройки сборки

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,12 @@ export default defineConfig(({ mode }) => {
       outDir: 'dist',
       minify: 'terser',
       sourcemap: false,
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      },
       rollupOptions: {
         output: {
           manualChunks: undefined
